f6d3191ee159436c11c7d8acb1b344758196382f
tdk246 <127329430+tdk246@users.noreply.github.com>
2025-07-15 17:38:21 +0700

    fix error login with gg

--- App.js
+++ App.js
@@ -50,7 +50,11 @@ app.use(
     secret: process.env.SECRET_KEY || "your_session_secret",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000
+    },
   })
 );
 
